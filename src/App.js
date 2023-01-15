8cae74b80b612f19cb2a781249613520838244fd
Slavcho Boyukliev <boyuklievslavcho@gmail.com>
2023-01-15 16:35:30 +0200

    Load all games in catalog from server

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import * as gameService from './services/gameService';
+
 import Catalog from './components/Catalog/Catalog';
 import CreateGame from './components/CreateGame/CreateGame';
 import Header from './components/Header/Header';
@@ -6,18 +9,29 @@ import Home from './components/Home/Home';
 import Login from './components/Login/Login';
 import Register from './components/Register/Register';
 
+
+
 function App() {
+    const [games, setGames] = useState([]);
+
+    useEffect(() => {
+        gameService.getAll()
+            .then(result => {
+                setGames(result);
+            });
+    }, []);
+
     return (
         <div className="app-wrapper">
             <Header />
 
             <main id="main-content">
                 <Routes>
-                    <Route path="/" element={<Home />} />
+                    <Route path="/" element={<Home games={games}/>} />
                     <Route path="/login" element={<Login />} />
                     <Route path="/register" element={<Register />} />
                     <Route path="/create" element={<CreateGame />} />
-                    <Route path="/catalog" element={<Catalog />} />
+                    <Route path="/catalog" element={<Catalog games={games} />} />
                 </Routes>
             </main>
         </div>
--- src/components/Catalog/Catalog.js
+++ src/components/Catalog/Catalog.js
@@ -1,22 +1,17 @@
+import CatalogItem from './CatalogItem/CatalogItem';
+
 /* eslint-disable jsx-a11y/alt-text */
-const Catalog = () => {
+const Catalog = ({games}) => {
     return (
         <section id="catalog-page">
             <h1>All Games</h1>
             {/* Display div: with information about every game (if any) */}
-            <div className="allGames">
-                <div className="allGames-info">
-                    <img src="./images/avatar-1.jpg" />
-                    <h6>Action</h6>
-                    <h2>Cover Fire</h2>
-                    <a href="/#" className="details-button">
-                        Details
-                    </a>
-                </div>
-            </div>
+            {games.length > 0
+            ? games.map(el => <CatalogItem game={el} />)
+            : <h3 className="no-articles">No articles yet</h3>
+            }
             
             {/* Display paragraph: If there is no games */}
-            <h3 className="no-articles">No articles yet</h3>
         </section>
     );
 };
