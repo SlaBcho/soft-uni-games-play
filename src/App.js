581768a901689fbcbc73443abccb1848f21b0055
Slavcho Boyukliev <boyuklievslavcho@gmail.com>
2023-01-15 16:02:33 +0200

    Add Login and Register page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { Routes, Route } from 'react-router-dom';
 import Header from './components/Header/Header';
 import Home from './components/Home/Home';
+import Login from './components/Login/Login';
+import Register from './components/Register/Register';
 
 function App() {
     return (
@@ -10,6 +12,8 @@ function App() {
             <main id="main-content">
                 <Routes>
                     <Route path="/" element={<Home />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/register" element={<Register />} />
                 </Routes>
             </main>
         </div>
