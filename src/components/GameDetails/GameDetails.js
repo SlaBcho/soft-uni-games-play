63761b026001270f67fe7e4a3340ed0ac8b0b1f7
Slavcho Boyukliev <boyuklievslavcho@gmail.com>
2023-01-15 17:12:39 +0200

    Add comment to game state

--- src/components/GameDetails/GameDetails.js
+++ src/components/GameDetails/GameDetails.js
@@ -1,14 +1,40 @@
 /* eslint-disable jsx-a11y/alt-text */
-const GameDetails = () => {
-    return(
+import { useParams } from 'react-router-dom';
+import { useState } from 'react';
+
+const GameDetails = ({ 
+    games,
+    addComment
+ }) => {
+    const { gameId } = useParams();
+    const [comment, setComment] = useState({
+        username: '',
+        comment: ''
+    });
+
+    const game = games.find(el => el._id === gameId);
+
+    const addCommentHandler = (e) => {
+        e.preventDefault();
+        addComment(gameId, `${comment.username}: ${comment.comment}`);
+    };
+
+    const onChange = (e) => {
+        setComment(state => ({
+            ...state,
+            [e.target.name]: e.target.value,
+        }));
+    };
+
+    return (
         <section id="game-details">
             <h1>Game Details</h1>
             <div className="info-section">
                 <div className="game-header">
-                    <img className="game-img" src="images/MineCraft.png" />
-                    <h1>Bright</h1>
-                    <span className="levels">MaxLevel: 4</span>
-                    <p className="type">Action, Crime, Fantasy</p>
+                    <img className="game-img" src={game.imageUrl} />
+                    <h1>{game.title}</h1>
+                    <span className="levels">MaxLevel: {game.maxLevel}</span>
+                    <p className="type">{game.category}</p>
                 </div>
                 <p className="text">
                     Set in a world where fantasy creatures live side by side with humans. A
@@ -46,9 +72,25 @@ const GameDetails = () => {
             {/* Add Comment ( Only for logged-in users, which is not creators of the current game ) */}
             <article className="create-comment">
                 <label>Add new comment:</label>
-                <form className="form">
-                    <textarea name="comment" placeholder="Comment......" defaultValue={""} />
-                    <input className="btn submit" type="submit" defaultValue="Add Comment" />
+                <input
+                    name="username"
+                    type="text"
+                    placeholder="John Doe"
+                    onChange={onChange}
+                    value={comment.username}
+                />
+                <form className="form" onSubmit={addCommentHandler}>
+                    <textarea
+                        name="comment"
+                        placeholder="Comment......" 
+                        onChange={onChange}
+                        value={comment.comment}
+                        />
+                    <input
+                        className="btn submit"
+                        type="submit"
+                        value="Add Comment"
+                    />
                 </form>
             </article>
         </section>
